b3553ad2cd67d4496ce091ab65029b77634293c0
Clemence <clemence.thierry60@orange.fr>
2022-10-02 23:26:24 -0400

    Introduction fonctionnelle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
+document.querySelector("#firstPart").classList.add("none");
+
+document.querySelector("#startGame").addEventListener('click', function() {
+    document.querySelector("#leftDoor").style.animation = "leftDoorOpening 1s linear forwards";
+    document.querySelector("#rightDoor").style.animation = "rightDoorOpening 1s linear forwards";
+    document.querySelector("#startGame").style.opacity = "0";
+});
+
+document.querySelector("#rightDoor").addEventListener("animationend", function() {
+    document.querySelector("#accueil").style.display = "none";
+    document.querySelector("#messageIntro > img").style.animation = "wiggle 1s linear infinite";
+}, false);
+
+document.querySelector("#messageIntro > img").addEventListener("click", function() {
+    this.classList.add("none");
+    document.querySelector("#messageIntro > h1").classList.add("none");
+    document.querySelector("#contenuMessage").classList.remove("none");
+
+    // document.querySelector("#contenuMessage").style.height = "50%";
+    document.querySelector("#contenuMessage").style.transform = "scale(1)";
+    setTimeout(function() {
+        document.querySelector("#contenuMessage>button").style.opacity = "1";
+    }, 8000);
+});
+
+document.querySelector("#contenuMessage>button").addEventListener("click", function() {
+    document.querySelector("#messageIntro").style.opacity = "0";
+    setTimeout(function() {
+        document.querySelector("#messageIntro").classList.add("none");
+        document.querySelector("#firstPart").classList.remove("none");
+    }, 200);
+
+    setTimeout(function() {
+        document.querySelector("#firstPart .next").style.opacity = "1";
+    }, 5000);
+});
+
+document.querySelector("#firstPart .next").addEventListener("click", function() {
+    // document.querySelector("#firstPart").classList.add("none");
+    document.querySelectorAll(".firstPlan").forEach(function(element) {
+        element.classList.add("none");
+    })
+});
+
 const optionsDiv = document.querySelector("#MyDialogue");
-const questionDiv = document.querySelector("#PNJdialogue");
+const questionDiv = document.querySelector("#blackBoard");
 let questions = [];
 let index = 0;
 let nbErreur = 0;
@@ -15,12 +59,9 @@ fetch('../json/questions.json')
         console.error(err);
     });
 
-document.querySelector("#PNJdialogue").addEventListener("click", isAnswerCorrect);
+document.querySelector("#blackBoard").addEventListener("click", isAnswerCorrect);
 
 function startGame() {
-
-    console.log("le jeu peut commencer ! ");
-    console.log(questions.length);
     askQuestion(questions[index]);
     // if (isAnswerCorrect) {
     //     alert("dommage")
@@ -77,13 +118,3 @@ function nextQuestion() {
         console.log("Plus de questions.");
     }
 }
-
-document.querySelector("#startGame").addEventListener('click', function() {
-    document.querySelector("#leftDoor").style.animation = "leftDoorOpening 1s linear forwards";
-    document.querySelector("#rightDoor").style.animation = "rightDoorOpening 1s linear forwards";
-    document.querySelector("#startGame").style.opacity = "0";
-});
-
-document.querySelector("#rightDoor").addEventListener("animationend", function() {
-    document.querySelector("#accueil").style.display = "none";
-}, false);
